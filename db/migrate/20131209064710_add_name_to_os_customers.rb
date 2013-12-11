815c3707116cf147b0fc3ed1cf6ecf209698837d
Avnesh Shakya <avnesh@neevtech.com>
2013-12-11 16:11:14 +0530

    deleted old migrate

--- db/migrate/20131209064710_add_name_to_os_customers.rb
+++
@@ -1,7 +0,0 @@
-class AddNameToOsCustomers < ActiveRecord::Migration
-  def change
-    add_column :os_customers, :cust_name, :string
-    add_column :os_customers, :cust_phone_no, :integer
-    add_column :os_customers, :cust_type, :string
-  end
-end
