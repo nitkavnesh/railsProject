90f366626ac998f93595b772f1bf54070cc96d8d
Avnesh Shakya <avnesh@neevtech.com>
2013-12-12 19:37:22 +0530

    after changing the db model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnlineShoping::Application.config.secret_key_base = 'fe4c675ca1fdb9a830a4ce139bdbec46a6136766b23c1ce1660718284499a240b79788cd7147336418596408c9ef7640028f311d15ab20b7454e111e2b1fe1b4'
