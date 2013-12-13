41deec7d34d30170010fac20fd888c984e6b0248
hemant <hemant@neevtech.com>
2013-12-13 16:48:32 +0530

    fixed indentation

--- online_shopping/app/models/user.rb
+++ online_shopping/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         belongs_to :role
-         has_many :bills
-         has_many :addresses
+  belongs_to :role
+  has_many :bills
+  has_many :addresses
 end
