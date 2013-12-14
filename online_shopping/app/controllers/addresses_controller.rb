77249217406b0a9fdeef90389a2967721a317ddb
Avnesh Shakya <avnesh@neevtech.com>
2013-12-14 14:20:52 +0530

    address operation completed

--- online_shopping/app/controllers/addresses_controller.rb
+++ online_shopping/app/controllers/addresses_controller.rb
@@ -1,5 +1,6 @@
 class AddressesController < ApplicationController
   def index
+  	@addresses = Address.all
   end
 
   def new
@@ -7,6 +8,26 @@ class AddressesController < ApplicationController
   end
 
   def create
-  	
+  	@address = Address.new(get_address_parameter).save!
+  	redirect_to :addresses
+  end
+
+  def show
+  	@address = Address.find(params[:id])
+  end
+
+  def edit
+  	@address = Address.find(params[:id])
+  end
+
+  def destroy
+  	@address = Address.find(params[:id])
+  	@address.destroy
+  	redirect_to addresses_path
+  end
+
+  private 
+  def get_address_parameter 
+  	params.require(:address).permit(:user_id, :address, :landmark, :city, :state, :country, :pincode);
   end
 end
