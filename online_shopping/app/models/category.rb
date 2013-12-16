6c0c1876eba2950628fa220b23645040802262a2
Avnesh Shakya <avnesh@neevtech.com>
2013-12-16 17:16:15 +0530

    added image in categories

--- online_shopping/app/models/category.rb
+++ online_shopping/app/models/category.rb
@@ -2,5 +2,6 @@ class Category < ActiveRecord::Base
 
 	has_many :products
 	has_many :subcategory, :class_name => "Category", :foreign_key => "parent_id"
-    belongs_to :parentcategory, :class_name => "Category"
+  belongs_to :parentcategory, :class_name => "Category"
+  has_attached_file :image
 end
