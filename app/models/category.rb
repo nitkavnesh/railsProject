8cb103f6eb74707d0792dd744cf8c6d2be4f21d3
hemant <hemant@neevtech.com>
2013-12-24 18:28:22 +0530

    default image added

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,7 +5,7 @@ class Category < ActiveRecord::Base
 	has_many :products
 	has_many :subcategory, :class_name => "Category", :foreign_key => "parent_id"
   belongs_to :parentcategory, :class_name => "Category"
-  has_attached_file :image
+  has_attached_file :image, :default_url => "/images/default-product.png"
 searchable do
     text :name
   end
